b3e451c96e97ff8857a96917624a489e93c1dfa8
Rabia Demiroğ <77547046+rabiademirog@users.noreply.github.com>
2024-11-27 00:49:05 +0300

    Add files via upload

---
+++ Controllers/MusteriController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db= new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Sil(int id)
+        {
+            var musteri= db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mstr = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mstr);
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var mus = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            mus.MUSTERIAD = p1.MUSTERIAD;
+            mus.MSTERISOYAD = p1.MSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
